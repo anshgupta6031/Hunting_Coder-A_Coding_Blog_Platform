0793e4162d235c1fd9e0f07a12658f02e0c8ab38
Ansh Gupta <guptaansh6031@gmail.com>
2024-08-09 18:47:39 +0530

    video 20 completed

--- app/blogpost/[slug]/page.js
+++ app/blogpost/[slug]/page.js
@@ -1,20 +1,26 @@
+"use client"
 
 
 
-
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from "./page.module.css"
 
 
 export default function Page({ params }) {
+
+    const [blog, setBlog] = useState({})
+
+    useEffect(() => {
+        fetch(`http://localhost:3000/api/getblog/${params.slug}`)
+            .then((a) => { return a.json() })
+            .then((data) => { setBlog(data) })
+    }, [])
+
     return (
         <>
             <div className={styles.container}>
-                <h2 className={styles.heading}>Title of the Blog : {params.slug}</h2>
-
-                <div className={styles.content}>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati ipsum ut sequi quod earum architecto minus repellendus eius laboriosam! Quos recusandae sunt sint, quasi ducimus unde vitae in modi temporibus numquam fugit, esse sit placeat odit, eius debitis molestiae natus repellat nulla fugiat cumque atque est dolor? Necessitatibus perferendis nam iure. Repellat veniam maxime eum animi beatae nemo excepturi labore necessitatibus aliquam mollitia saepe totam tempore, earum doloremque impedit alias officia officiis in doloribus ducimus. Harum nostrum corrupti amet enim consectetur perferendis velit obcaecati quis, molestias facere magni iusto eligendi minima minus ullam fugit adipisci labore ut? Vel asperiores id dicta aliquam eaque saepe, voluptatem ipsum commodi cupiditate nisi nostrum quod impedit? Corrupti labore, porro veniam vel rem sequi dolorem sit voluptatibus iure placeat eos quis quia quidem dolorum corporis repellendus, nulla dignissimos officiis id nesciunt! Ea, fuga officiis in ducimus unde tempora enim sapiente minima quia laudantium animi aliquam iusto quas doloremque aspernatur architecto consequuntur dolore saepe? Molestiae, recusandae excepturi. Molestiae vero rem voluptatem dignissimos modi quisquam tempore error eos consectetur, necessitatibus laudantium ut assumenda magni obcaecati fugit repudiandae eum mollitia amet fuga? Molestias ab, ullam soluta incidunt beatae nihil nostrum magni, accusamus similique obcaecati minus dignissimos fuga ea commodi laboriosam reiciendis repudiandae repellendus libero eveniet ad id culpa voluptas ut! Minima voluptas accusantium obcaecati, officia molestias asperiores delectus. At tempora ratione iste reiciendis aspernatur numquam recusandae. Fugiat inventore nostrum modi qui nemo, aspernatur eum aut illum quae. Quis explicabo libero modi veritatis non inventore ratione placeat quod, nisi soluta? Tempora dignissimos quaerat nisi obcaecati eos explicabo distinctio eligendi voluptas iusto? Numquam soluta, expedita placeat vitae laudantium provident voluptatibus. Optio quisquam eaque natus tempora voluptatem. Perspiciatis unde autem nihil perferendis est doloremque numquam blanditiis, reprehenderit incidunt pariatur harum corporis dolore corrupti fuga nesciunt necessitatibus laboriosam beatae commodi asperiores eius nemo doloribus tempore, quod dolorum. Iste itaque nam est impedit ipsum, delectus quas consequatur suscipit, eaque praesentium amet nulla magnam mollitia id. Omnis obcaecati, cumque dolore delectus vel illo quibusdam magni modi a sed quia praesentium. Architecto sit accusamus autem quod? Excepturi voluptas, officia illo quos voluptatum accusantium ratione saepe ducimus quibusdam sequi atque a eos unde! Qui in consequuntur non inventore dolores consequatur maxime rem culpa facilis deleniti aliquam harum debitis quos fugiat nam molestias dolorem est ipsum eligendi adipisci, impedit totam, quam, tempore odio. Illo vel quaerat rerum perferendis ut corrupti non minima. Iusto in error voluptas consequatur, amet commodi. Inventore quaerat reprehenderit praesentium laborum beatae odit asperiores assumenda fuga vel. Inventore eius alias rem possimus tenetur obcaecati et amet molestias, aperiam nesciunt dolorum, nisi ipsum itaque voluptates quasi pariatur vero debitis nemo dolores, saepe quisquam temporibus voluptatem unde. Alias a sapiente facilis rerum dolor! In fugiat consectetur maxime, similique molestias fugit nam laborum sit unde eius cupiditate et magni corrupti doloribus tempora, eveniet enim dignissimos itaque tempore assumenda voluptates temporibus. Adipisci, dignissimos culpa? Necessitatibus atque ea autem minima aperiam. Praesentium voluptates, pariatur eaque nulla expedita ab sequi. Architecto magnam itaque omnis eius aliquam perspiciatis et reprehenderit obcaecati?
-                </div>
+                <h2 className={styles.heading}>Title : {blog.title}</h2>
+                <div className={styles.content}>{blog.content}</div>
             </div>
         </>
     )
